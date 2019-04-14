2bf6512205cdf7786b7d69e2a2b0028b2cbe6a63
Bryant <cabrera.bryant@gmail.com>
2019-04-14 02:54:17 -0700

    Bryant updates Wallet.js component layout and styling based on new UX mid-fi/hi-fi wireframe.

--- src/Components/Wallet/Wallet.js
+++ src/Components/Wallet/Wallet.js
@@ -5,53 +5,38 @@ import ModalSection from '../Modal/ModalSection'
 
 export default class Wallet extends Component {
     state = {
-        earningsTotal: 3922.23,
-        betsTotal: 1829.21,
+        depositTotal: 1105.02,
+        netGainTotal: 2817.21,
+        balance: 3922.23,
         graphData: [{ name: 'date1', earnings: 400, bets: 2400, amt: 2400 }, { name: 'date2', earnings: 200, bets: 1200, amt: 2000 }]
     }
 
     render() {
         return (
-            <div className="earnings">
-                <div className="earnings__earnings totals-box">
-                    <h3 className="totals-box__title">Earnings:</h3>
+            <div className="wallet">
+                <div className="wallet__earnings totals-box">
+                    <h2 className="totals-box__title">Lifetime Balance</h2>
 
-                    <ul className="totals-box__toggle">
-                        <li><a href="#">Week</a></li>
-                        <li><a href="#">Month</a></li>
-                        <li><a href="#">Total</a></li>
-                    </ul>
+                    <div className="wallet__deposit totals-box__item">
+                        <h3>Deposit:</h3>
+                        <h3>${this.state.depositTotal}</h3>
+                    </div>
 
-                    <div className="totals-box__total">
-                        <h2>${this.state.earningsTotal}</h2>
+                    <div className="wallet__net-gain totals-box__item">
+                        <h3>Net Gain:</h3>
+                        <h3>${this.state.netGainTotal}</h3>
+                    </div>
+
+                    <div className="wallet__balance totals-box__item">
+                        <h2>Balance:</h2>
+                        <h2>${this.state.balance}</h2>
                     </div>
                     
-                    <ModalSection />
-                    <button className="totals-box__btn">Cash Out</button>
+                    <ModalSection className="wallet__btn" />
                 </div>
 
-                <div className="earnings__bets totals-box">
-                    <h3 className="totals-box__title">Bets:</h3>
-
-                    <ul className="totals-box__toggle">
-                        <li><a href="#">Week</a></li>
-                        <li><a href="#">Month</a></li>
-                        <li><a href="#">Total</a></li>
-                    </ul>
-
-                    <div className="totals-box__total">
-                        <h2>${this.state.betsTotal}</h2>
-                    </div>
-
-
-                    <ModalSection />
-                    <button className="totals-box__btn">Cash Out</button>
-
-
-                </div>
-
-                <div className="earnings__graph">
-                    <ul className="earnings__graph__toggle totals-box__toggle">
+                <div className="wallet__graph">
+                    <ul className="wallet__graph__toggle totals-box__toggle">
                         <li><a href="#">Week</a></li>
                         <li><a href="#">Month</a></li>
                         <li><a href="#">Total</a></li>
@@ -66,6 +51,60 @@ export default class Wallet extends Component {
                     </ResponsiveContainer>
                 </div>
             </div>
+            // old prototype //<div className="earnings">
+            //     <div className="earnings__earnings totals-box">
+            //         <h3 className="totals-box__title">Earnings:</h3>
+
+            //         <ul className="totals-box__toggle">
+            //             <li><a href="#">Week</a></li>
+            //             <li><a href="#">Month</a></li>
+            //             <li><a href="#">Total</a></li>
+            //         </ul>
+
+            //         <div className="totals-box__total">
+            //             <h2>${this.state.earningsTotal}</h2>
+            //         </div>
+                    
+            //         <ModalSection />
+            //         <button className="totals-box__btn">Cash Out</button>
+            //     </div>
+
+            //     <div className="earnings__bets totals-box">
+            //         <h3 className="totals-box__title">Bets:</h3>
+
+            //         <ul className="totals-box__toggle">
+            //             <li><a href="#">Week</a></li>
+            //             <li><a href="#">Month</a></li>
+            //             <li><a href="#">Total</a></li>
+            //         </ul>
+
+            //         <div className="totals-box__total">
+            //             <h2>${this.state.betsTotal}</h2>
+            //         </div>
+
+
+            //         <ModalSection />
+            //         <button className="totals-box__btn">Cash Out</button>
+
+
+            //     </div>
+
+            //     <div className="earnings__graph">
+            //         <ul className="earnings__graph__toggle totals-box__toggle">
+            //             <li><a href="#">Week</a></li>
+            //             <li><a href="#">Month</a></li>
+            //             <li><a href="#">Total</a></li>
+            //         </ul>
+            //         <ResponsiveContainer maxWidth={400} maxHeight={400} minHeight={100}>
+            //             <LineChart width={725} height={300} data={this.state.graphData}>
+            //                 <XAxis dataKey='name' />
+            //                 <CartesianGrid stroke="#ccc" strokeDasharray='5 5' />
+            //                 <Tooltip />
+            //                 <Line type='monotone' dataKey='earnings' stroke='#0000ff' />
+            //             </LineChart>
+            //         </ResponsiveContainer>
+            //     </div>
+            // </div>
         );
     }
 }
